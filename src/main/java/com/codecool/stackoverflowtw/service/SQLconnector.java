efc5b9759a0621bbc29b77292f172d9bd8018e22
feldicskobalazs <111083764+feldicskobalazs@users.noreply.github.com>
2023-08-09 10:31:32 +0200

    add sqlconnector

---
+++ src/main/java/com/codecool/stackoverflowtw/service/SQLconnector.java
@@ -0,0 +1,24 @@
+package com.codecool.stackoverflowtw.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLconnector {
+    private final String url = "jdbc:postgresql://localhost:5432/stackoverflow-twproject";
+    private final String user = "postgres";
+    private final String password = "372969";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+}
