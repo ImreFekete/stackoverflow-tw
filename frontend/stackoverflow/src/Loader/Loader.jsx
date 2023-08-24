7bc97ab532a85adce04c01d5b00ab7573a328001
Fekete Imre <feketeimi12@gmail.com>
2023-08-24 11:25:53 +0200

    add Loader element

---
+++ frontend/stackoverflow/src/Loader/Loader.jsx
@@ -0,0 +1,23 @@
+import "./Loader.css";
+const Loader = () => {
+    return <div className="scene">
+        <div className="shadow"></div>
+        <div className="jumper">
+            <div className="spinner">
+                <div className="scaler">
+                    <div className="loader">
+                        <div className="cuboid">
+                            <div className="cuboid__side"></div>
+                            <div className="cuboid__side"></div>
+                            <div className="cuboid__side"></div>
+                            <div className="cuboid__side"></div>
+                            <div className="cuboid__side"></div>
+                            <div class="cuboid__side"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+export default Loader;
